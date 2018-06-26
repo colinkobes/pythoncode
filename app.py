31338aed65734e03fb3edae3b23dd685115d0a8a
colinkobes <40594340+colinkobes@users.noreply.github.com>
2018-06-26 11:06:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,16 +15,16 @@ def home():
     return render_template('home.html')
 
 def rest_request_example():
-    print (requests.get("http://140.86.15.104:3000/fighters/45/0/pink/colinkobes").text)
-    print (requests.get("http://140.86.15.104:3000/fighters/45/1/pink/colinkobes").text)
-    print (requests.get("http://140.86.15.104:3000/fighters/45/2/pink/colinkobes").text)
-    print (requests.get("http://140.86.15.104:3000/fighters/45/3/pink/colinkobes").text)
-    print (requests.get("http://140.86.15.104:3000/fighters/45/4/pink/colinkobes").text)
-    print (requests.get("http://140.86.15.104:3000/fighters/45/5/pink/colinkobes").text)
-    print (requests.get("http://140.86.15.104:3000/fighters/45/6/pink/colinkobes").text)
-    print (requests.get("http://140.86.15.104:3000/fighters/45/7/pink/colinkobes").text)
-    print (requests.get("http://140.86.15.104:3000/fighters/45/8/pink/colinkobes").text)
-    print (requests.get("http://140.86.15.104:3000/fighters/45/9/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/reactorCore/45/0/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/reactorCore/45/1/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/reactorCore/45/2/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/reactorCore/45/3/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/reactorCore/45/4/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/reactorCore/45/5/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/reactorCore/45/6/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/reactorCore/45/7/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/reactorCore/45/8/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/reactorCore/45/9/pink/colinkobes").text)
 
 
 def read_db_SQL_example():
