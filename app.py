e8d3046e36cff51bea92d7e1fd909a285dfd7a23
colinkobes <40594340+colinkobes@users.noreply.github.com>
2018-06-26 10:58:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,17 @@ def home():
     return render_template('home.html')
 
 def rest_request_example():
-    print (requests.get("http://140.86.15.104:3000/shield/33/45/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/fighters/45/0/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/fighters/45/1/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/fighters/45/2/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/fighters/45/3/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/fighters/45/4/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/fighters/45/5/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/fighters/45/6/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/fighters/45/7/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/fighters/45/8/pink/colinkobes").text)
+    print (requests.get("http://140.86.15.104:3000/fighters/45/9/pink/colinkobes").text)
+
 
 def read_db_SQL_example():
     conn = db.get_engine().connect()
